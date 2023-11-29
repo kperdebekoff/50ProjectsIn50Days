40f45c261be4641554ce8392dac26f5ae79b6e6f
Kayyrbek Perdebekov <kperdebekoff@gmail.com>
2023-11-29 23:37:37 +0500

    Delete unnecessary code

--- 06-scroll-animation/script.js
+++ 06-scroll-animation/script.js
@@ -6,11 +6,9 @@ checkBoxes();
 
 function checkBoxes() {
   const triggerBottom = (window.innerHeight / 5) * 4;
-  console.log(triggerBottom);
 
   boxes.forEach((box) => {
     const boxTop = box.getBoundingClientRect().top;
-    console.log(boxTop);
 
     if (boxTop < triggerBottom) {
       box.classList.add('show');
